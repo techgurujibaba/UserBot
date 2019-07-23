2834c40779969502343b71d57d1dada73d324130
Shivam Choudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2019-07-23 23:45:08 +0530

    Update chat.py

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -7,6 +7,8 @@
 from time import sleep
 
 from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
 
 from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID, bot
 from userbot.events import register
@@ -44,6 +46,27 @@ async def chatidgetter(chat):
         await chat.edit("Chat ID: `" + str(chat.chat_id) + "`")
 
 
+@register(outgoing=True, pattern="^.mention (.*)")
+async def mention(event):
+    """ For .chatid, returns the ID of the chat you are in at that moment. """
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        if event.fwd_from:
+            return
+        input_str = event.pattern_match.group(1)
+        if event.reply_to_msg_id:
+            previous_message = await event.get_reply_message()
+            if previous_message.forward:
+                replied_user = previous_message.forward.from_id
+            else:
+                replied_user = previous_message.from_id
+        else:
+            return
+        user_id = replied_user
+        caption = """<a href='tg://user?id={}'>{}</a>""".format(
+            user_id, input_str)
+        await event.edit(caption, parse_mode="HTML")
+
+
 @register(outgoing=True, pattern=r"^.log(?: |$)([\s\S]*)")
 async def log(log_text):
     """ For .log command, forwards a message or the command argument to the bot logs group """
@@ -74,17 +97,63 @@ async def kickme(leave):
         await bot(LeaveChannelRequest(leave.chat_id))
 
 
+@register(outgoing=True, pattern="^.unmutechat$")
+async def unmute_chat(unm_e):
+    """ For .unmutechat command, unmute a muted chat. """
+    if not unm_e.text[0].isalpha() and unm_e.text[0] not in ("/", "#", "@", "!"):
+        try:
+            from userbot.modules.sql_helper.keep_read_sql import unkread
+        except AttributeError:
+            await unm_e.edit('`Running on Non-SQL Mode!`')
+            return
+        unkread(str(unm_e.chat_id))
+        await unm_e.edit("```Unmuted this chat Successfully```")
+
+
+@register(outgoing=True, pattern="^.mutechat$")
+async def mute_chat(mute_e):
+    """ For .mutechat command, mute any chat. """
+    if not mute_e.text[0].isalpha() and mute_e.text[0] not in ("/", "#", "@", "!"):
+        try:
+            from userbot.modules.sql_helper.keep_read_sql import kread
+        except AttributeError:
+            await mute_e.edit("`Running on Non-SQL mode!`")
+            return
+        await mute_e.edit(str(mute_e.chat_id))
+        kread(str(mute_e.chat_id))
+        await mute_e.edit("`Shush! This chat will be silenced!`")
+        if BOTLOG:
+            await mute_e.client.send_message(
+                BOTLOG_CHATID,
+                str(mute_e.chat_id) + " was silenced.")
+
+
+@register(incoming=True)
+async def keep_read(message):
+    """ The mute logic. """
+    try:
+        from userbot.modules.sql_helper.keep_read_sql import is_kread
+    except AttributeError:
+        return
+    kread = is_kread()
+    if kread:
+        for i in kread:
+            if i.groupid == str(message.chat_id):
+                await message.client.send_read_acknowledge(message.chat_id)
+
 CMD_HELP.update({
-    "chatid": "Fetches the current chat's ID"
-})
-CMD_HELP.update({
-    "userid": "Fetches the ID of the user in reply, if its a \
-forwarded message, finds the ID for the source."
-})
-CMD_HELP.update({
-    "log": "Forwards the message you've replied to in your \
-bot logs group."
-})
-CMD_HELP.update({
-    "kickme": "Leave from a targeted group."
+    "chat": ".chatid\
+\nUsage: Fetches the current chat's ID\
+\n\n.userid\
+\nUsage: Fetches the ID of the user in reply, if its a forwarded message, finds the ID for the source.\
+\n\n.log\
+\nUsage: Forwards the message you've replied to in your bot logs group.\
+\n\n.kickme\
+\nUsage: Leave from a targeted group.\
+\n\n.unmutechat\
+\nUsage: Unmutes a muted chat.\
+\n\n.mutechat\
+\nUsage: Allows you to mute any chat.\
+\n\n.mention <text>\
+\nUsage: Reply to generate the user's permanent link with custom text."
 })
